b028f2e2bed76addc72839f0886544135dc16618
Nada Zelic <tsukuyomi@Nadas-MacBook-Pro.local>
2020-08-14 19:28:39 +0200

    Add eslint, prettier

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const morgan = require('morgan');
 
 const app = express();
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 
 app.get('/', (req, res) => {
