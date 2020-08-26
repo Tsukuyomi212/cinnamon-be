17faefc35bc521f037dfcff54ebe4c79d6b0b2de
Nada Zelic <tsukuyomi@Nadas-MacBook-Pro.local>
2020-08-26 11:38:53 +0200

    DB setup. Add DB seeder.

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to mongoDB!'));
 
 const app = require('./app');
 
