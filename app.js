ae82ae636c1b732138ed49502e11d3c8b8b5d5f7
Tsukuyomi212 <nada.zelic@gmail.com>
2021-10-03 19:27:00 +0200

    refactor

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3030;
 
-app.get('/', (req, res) => {
-  res.send('Hello Cinnamon');
-});
-
 app.listen(port, () => {
   console.log(`Cinammon app listening at http://localhost:${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello Cinnamon' });
 });
