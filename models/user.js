e884f4fcca8a54189bd83a8449c446ccd62f968c
Tsukuyomi212 <nada.zelic@gmail.com>
2021-10-05 09:28:24 +0200

    add basic user model

---
+++ models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
+  },
+  photo: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
