b7900cf6e3bca9711a44b16b100974317d51d42b
Tsukuyomi212 <nada.zelic@gmail.com>
2021-10-05 14:15:54 +0200

    add user signup and login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,13 +5,10 @@ const authController = require('./../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
+router.post('/login', authController.login);
 
-router.route('/').get(userController.getAllUsers).post(userController.createUser);
+router.route('/').get(userController.getAllUsers);
 
-router
-  .route('/:id')
-  .get(userController.getUser)
-  .patch(userController.updateUser)
-  .delete(userController.deleteUser);
+router.route('/:id').get(userController.getUser).delete(userController.deleteUser);
 
 module.exports = router;
