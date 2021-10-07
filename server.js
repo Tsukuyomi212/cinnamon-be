a9a051135a50961c4cd4152594b97cc69abdada0
Tsukuyomi212 <nada.zelic@gmail.com>
2021-10-07 12:47:32 +0200

    fix eslint errors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT;
 
 mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
-app.listen(PORT || 8000, () => {
+const server = app.listen(PORT || 8000, () => {
   console.log(`Cinammon app listening at http://localhost:${PORT}`);
 });
 
