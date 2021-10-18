b730d5e3259a9ffb244e396ec5eda9eb9b6754ca
Tsukuyomi212 <nada.zelic@gmail.com>
2021-10-18 10:33:28 +0200

    Add cors

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const cors = require('cors');
+
+const authRouter = require('./routes/authRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,7 +11,11 @@ const app = express();
 //middlewares
 app.use(express.json());
 
+app.use(cors());
+app.options('*', cors());
+
 //routes
+app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
